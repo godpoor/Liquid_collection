be31505db7c921e708e1b3f013a569c0faed373c
godpro <godpoor@163.com>
2024-03-20 22:11:40 +0800

    0320/mac air I finish the RECT COUNT

---
+++ RECT_PNG.py
@@ -0,0 +1,42 @@
+# 讲一个正常的图像进行边缘检测，目前已经实现边缘检测以及保存png图像，还比较客观
+import cv2
+global filtered_bboxs
+# file = "other/BTSTO_176.png"
+file = "output_image.png"
+image = cv2.imread(file)
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# 利用二值法将图像的边框画出来
+_, threshold_image = cv2.threshold(gray_image, 40, 255, cv2.THRESH_BINARY)
+contours, _ = cv2.findContours(threshold_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contour_image = image.copy()
+# 使用矩形显示
+bboxs = [cv2.boundingRect(cnt) for cnt in contours]
+
+# 使用列表排序按照第三项加上第四项的值进行排序
+sorted_bboxs = sorted(bboxs, key=lambda x: x[2] + x[3], reverse=True)
+filtered_bboxs = [sorted_bboxs[0]]
+# 如果bboxs中有多个元素，保留第三项加上第四项最大的元素
+if len(sorted_bboxs) > 1:
+    filtered_bboxs = [sorted_bboxs[0]]  # 先将第一个元素加入筛选后的列表中
+    max_sum = sorted_bboxs[0][2] + sorted_bboxs[0][3]  # 记录第一个元素的第三项加上第四项的和
+    for bbox in sorted_bboxs[1:]:
+        if bbox[2] + bbox[3] > max_sum:
+            filtered_bboxs = [bbox]  # 如果当前元素的第三项加上第四项的和大于之前记录的最大和，则更新筛选后的列表
+            max_sum = bbox[2] + bbox[3]
+        elif bbox[2] + bbox[3] == max_sum :
+            filtered_bboxs.append(bbox)  # 如果当前元素的第三项加上第四项的和等于最大和，则将其加入筛选后的列表中
+    print("Filtered Bboxes:", filtered_bboxs)
+
+for x, y, w, h in filtered_bboxs:
+    print("x的值为：", x, "\ty的值为：", y, "\tw的值为：", w, "\th的值为：", h)
+    contour_image = cv2.rectangle(contour_image, (x, y), (x + w, y + h), (255, 0, 0), thickness=2)
+# 将对应的带有框框的图像进行保存
+cv2.imwrite('contours.png', contour_image)
+
+# 用于显示
+cv2.imshow('Image with Contours', contour_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
---
+++ tif_png.py
@@ -0,0 +1,45 @@
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+# 打开TIF图像
+img = Image.open('other/BTSTO_176.tif')
+# 将图像转换为NumPy数组
+img = np.array(img)
+
+# 获取图像的高度和宽度
+height, width = img.shape
+print(height, width)
+for i in range(height):
+    for j in range(width):
+        if img[i,j]<0:
+            img[i, j]=0
+
+#整数部分的所有的值
+data_max = 30
+data_min = 0  # 负数已经截断为0
+
+# # # 将正数线性映射到0-255范围
+norm_data = (img - data_min) / (data_max - data_min) * 255
+norm_data = norm_data.astype(np.uint8)
+
+plt.figure()
+plt.imshow(norm_data,cmap='gray',vmin=0,vmax=100)
+# 关闭坐标轴
+plt.axis('off')
+# 保存图像为PNG格式
+plt.savefig('output_image.png', bbox_inches='tight')
+# img = Image.fromarray(img)
+# img.save('output_image.png')
+plt.show()
+# # 获取正数部分的最大最小值
+# data_max = data.max()
+# data_min = 0  # 负数已经截断为0
+#
+# # # 将正数线性映射到0-255范围
+# # norm_data = (data - data_min) / (data_max - data_min) * 255
+# # norm_data = norm_data.astype(np.uint8)
+#
+# # 使用PIL保存为PNG
+# img = Image.fromarray(img)
+# # img.save('output_image.tif')
+# img.save('output_image.png')
